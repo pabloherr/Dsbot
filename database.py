a009234e5ff1fff5a626e71d3f795bd514504462
pabloherr <pabloherrera234@gmail.com>
2024-03-15 17:35:22 -0300

    Update database connection string

--- database.py
+++ database.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 
 db_client = MongoClient().local
 
-#db_client = MongoClient("mongodb+srv://pabloherrera234:Losmaristas1999@pipobot.b3kejus.mongodb.net/?retryWrites=true&w=majority&appName=PipoBot")
+#db_client = MongoClient("mongodb+srv://pabloherrera234:Losmaristas1999@pipobot.t8x4ipz.mongodb.net/?retryWrites=true&w=majority&appName=PipoBot").pipobot
