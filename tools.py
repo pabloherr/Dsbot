7424a9c352b49d04c6041661ede96b6689a42be5
pabloherr <pabloherrera234@gmail.com>
2024-03-17 03:05:53 -0300

    Remove unused code and refactor velocity calculation

--- tools.py
+++ tools.py
@@ -5,7 +5,6 @@ from models.pipo import Pipo
 from database import db_client
 
 db = db_client["discord"]
-channel = 1212525884070699079
 # Command to create a random pipo
 async def random_pipo( wild = False) -> Pipo:
     
@@ -75,69 +74,6 @@ async def lvlup(pipo: dict, stat1: str, stat2: str) -> dict:
     pipo["exp"] = 0
     return pipo
 
-# Command to calculate the velocity
-async def velocity(pipo1: dict, pipo2: dict):
-    ff1 = False
-    ff2 = False
-    if pipo1["passive"] == "Fight First":
-        prob = random.randint(1, 4)
-        if prob == 1:
-            ff1 = True
-            pipo1["speed"] += 2
-        else:
-            pipo1["speed"] -= 2
-    if pipo2["passive"] == "Fight First":
-        prob = random.randint(1, 4)
-        if prob == 1:
-            ff2 = True
-            pipo2["speed"] += 2
-        else:
-            pipo2["speed"] -= 2
-        
-    
-    #pipo1 faster
-    if pipo1['speed'] > pipo2['speed']:
-        if pipo1["passive"] == "Fight First":
-            if ff1:
-                pipo1["speed"] -= 2
-            else:
-                pipo1["speed"] += 2
-        if pipo2["passive"] == "Fight First":
-            if ff2:
-                pipo2["speed"] -= 2
-            else:
-                pipo2["speed"] += 2
-        return pipo1
-    #pipo2 faster
-    elif pipo1['speed'] < pipo2['speed']:
-        if pipo1["passive"] == "Fight First":
-            if ff1:
-                pipo1["speed"] -= 2
-            else:
-                pipo1["speed"] += 2
-        if pipo2["passive"] == "Fight First":
-            if ff2:
-                pipo2["speed"] -= 2
-            else:
-                pipo2["speed"] += 2
-        return pipo2
-    #speed tie
-    else:
-        if pipo1["passive"] == "Fight First":
-            if ff1:
-                pipo1["speed"] -= 2
-            else:
-                pipo1["speed"] += 2
-        if pipo2["passive"] == "Fight First":
-            if ff2:
-                pipo2["speed"] -= 2
-            else:
-                pipo2["speed"] += 2
-        r = random.randint(0, 1)
-        if r == 0:
-            return pipo1
-        else:
-            return pipo2
 
 # Command to calculate the velocity
 async def alt_velocity(pipo1: dict, pipo2: dict, pipo3:dict = None, pipo4:dict = None):
